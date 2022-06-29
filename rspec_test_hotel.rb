c42db523bed9b6617e9905c97a7a1e2b5b6cbbc4
Alex Wade <alexjohnwade@gmail.com>
2022-06-29 12:15:57 +1000

    playing around with rspec testing

---
+++ rspec_test_hotel.rb
@@ -0,0 +1,30 @@
+class Hotel
+    attr_accessor :guests
+
+    def initialize
+      @guests = []
+    end
+
+    def check_in_guest(guest_name)
+      guests << guest_name
+    end
+
+    def check_out_guest(guest_name)
+      guests.delete(guest_name)
+    end
+  end
+
+  describe Hotel do
+    it 'can check a guest in' do
+      hotel = Hotel.new
+      hotel.check_in_guest('George Harrison')
+      expect(hotel.guests).to include 'George Harrison'
+    end
+
+    it 'can check a guest out' do
+      hotel = Hotel.new
+      hotel.check_in_guest('Buddy Holly')
+      hotel.check_out_guest('Buddy Holly')
+      expect(hotel.guests).not_to include 'Buddy Holly'
+    end
+  end
